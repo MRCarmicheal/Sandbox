5a2f50b9837183e46deb5f86aaee7ef41f2d72ac
Wang qi <qi.wang5@my.jcu.edu.au>
2022-03-30 15:25:10 +0800

    Add docstrings to password check

--- Password check.py
+++ Password check.py
@@ -1,12 +1,20 @@
+"""
+Description : A simple Password Checker
+
+Name: Wang
+"""
+
 
 def is_valid_password(text):
+    """Check whather a given text has the correct password format"""
     return 8 <= len(text) <= 20
 
 
 def main():
+    """Start program"""
     new_password = "helloworld"
     print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
